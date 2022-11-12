d109ee218e42f777a11238e9ea3895ed047d4038
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2022-11-12 19:04:31 +0100

    feat: implemented unmount hook

--- src/hooks/unmount/useUnmount.ts
+++ src/hooks/unmount/useUnmount.ts
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
 import { Callback } from '../../types';
 
-export default function useUnmount(callback: Callback) {};
+export default function useUnmount(callback: Callback) {
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	useEffect(() => callback, []);
+};
