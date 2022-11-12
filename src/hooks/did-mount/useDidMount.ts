6ebf1eb50e8b63b2d633428162cb222b597592bd
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2022-11-12 18:46:51 +0100

    feat: implemented useDidMount

--- src/hooks/did-mount/useDidMount.ts
+++ src/hooks/did-mount/useDidMount.ts
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
 import { Callback } from '../../types';
 
-export default function useDidMount(callback: Callback) {};
+export default function useDidMount(callback: Callback) {
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	useEffect(callback, []);
+};
