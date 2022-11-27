644df125192b19490e407d4bb9b9d08336782302
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2022-11-27 15:26:28 +0100

    feat: implemented did-update

--- src/hooks/did-update/useDidUpdate.ts
+++ src/hooks/did-update/useDidUpdate.ts
@@ -1,4 +1,18 @@
+import { useEffect, useRef } from 'react';
 import { Callback } from '../../types';
 
 export default function useDidUpdate(callback: Callback, references: any[] = []) {
+
+	const isMounted = useRef(false);
+
+	useEffect(() => {
+
+		if (isMounted.current) {
+			callback();
+		} else {
+			isMounted.current = true;
+		}
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, references);
 };
